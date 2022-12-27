aaf372c0d0e3d5e7431d6ecb3dae894372752ada
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-12-27 14:58:45 +0100

    adding toolbar along with functionalities

---
+++ src/components/CarList/Toolbar/ListToolBar.tsx
@@ -0,0 +1,39 @@
+import { Flex, TextInput } from "@mantine/core";
+import { IconSearch } from "@tabler/icons";
+import useSearchStore from "../../../Store/useSearchStore";
+import PriceCathegoryPicker from "./Pickers/FilterPicker";
+import SortByPicker from "./Pickers/SortByPicker";
+
+function ListToolBar() {
+    const { searchingParams, setSearchingParams } = useSearchStore((state) => ({
+        setSearchingParams: state.setSearchingParams,
+        searchingParams: state.searchingParams,
+    }));
+
+    return (
+        <Flex
+            m={25}
+            mih={50}
+            gap="lg"
+            justify="space-between"
+            align="center"
+            direction="row"
+            wrap="wrap"
+        >
+            <TextInput
+                placeholder="Search by the car model"
+                icon={<IconSearch size={14} />}
+                sx={{ width: 400 }}
+                value={searchingParams}
+                onChange={(e) => setSearchingParams(e.target.value)}
+            />
+
+            <Flex gap={"md"}>
+                <SortByPicker />
+                <PriceCathegoryPicker />
+            </Flex>
+        </Flex>
+    );
+}
+
+export default ListToolBar;
