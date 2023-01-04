3c00fb884eb046d45334cf3a43dc92c87e54c85b
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2023-01-04 22:27:57 +0100

    added 404 page

--- src/pages/MissingPage.tsx
+++ src/pages/MissingPage.tsx
@@ -1,11 +1,33 @@
+import { Button, Container, Group, Title, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
+import useMissingPageStyles from "../styles/missingPageStyles";
 
 function MissingPage() {
+    const { classes } = useMissingPageStyles();
+
     return (
-        <div>
-            <h1>404 error</h1>
-            <Link to="/">get back to the home page</Link>
-        </div>
+        <Container className={classes.root}>
+            <div className={classes.label}>404</div>
+            <Title className={classes.title}>
+                You have found a secret place.
+            </Title>
+            <Text
+                color="dimmed"
+                size="lg"
+                align="center"
+                className={classes.description}
+            >
+                Unfortunately, this is only a 404 page. You may have mistyped
+                the address, or the page has been moved to another URL.
+            </Text>
+            <Group position="center">
+                <Link to="/">
+                    <Button variant="subtle" size="md">
+                        Take me back to home page
+                    </Button>
+                </Link>
+            </Group>
+        </Container>
     );
 }
 
